29392b138fb5dc0f7e6b2884bccb877ca5503d48
Italo Russan <italo.russan@csktech.com.br>
2020-06-17 16:04:56 -0300

    fix: aparencia das infos na listagem

--- src/components/GameList/index.jsx
+++ src/components/GameList/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {dateFormat} from '../../components/Conversion';
+import {FaBoxes, FaGamepad, FaCalendarAlt} from 'react-icons/fa';
 import NoImg from '../../assets/images/no-img.jpeg';
 import './games-list.scss';
 
@@ -16,16 +17,19 @@ export default function GameList(props) {
                     </figure>
                     <div className="card-body d-flex flex-column justify-content-between">
                         <div className="mb-2">
-                            <h5 className="card-title">{data.name}</h5>
-                            <p className="card-text">Nota: {data.rating !== 0 ? data.rating : 'não avaliado'}</p>
-                            <p className="card-text">Gênero:
+                            <h5 className="card-title mb-4">{data.name}</h5>
+                            <p className="card-text">
+                                <span className="title-info"><FaBoxes/> Gênero</span>
                                 {data.genres != '' ? (
                                     data.genres.map((item, i) => (
                                         <Link key={i} to={`/genero/${item.id}/${item.name}`} className="mx-1">{item.name}</Link>
                                     ))
                                 ) : (<span> N/D</span>)}
                             </p>
-                            <p className="card-text">Plataforma:
+                            <p className="card-text">
+                                <span className="title-info">
+                                    <FaGamepad/> Plataforma
+                                </span>
                                 {data.platforms !== null ? (
                                     data.platforms.map((item, i) => (
                                         <Link key={i} to={`/plataforma/${item.platform.id}/${item.platform.name}`} className="mx-1">{item.platform.name}</Link>
@@ -33,7 +37,9 @@ export default function GameList(props) {
                                 ) : (<span> N/D</span>)}
                             </p>
                             <p className="card-text">
-                                Lançamento: {data.released !== null ? (dateFormat(data.released)) : (<span> N/D</span>)}
+                                <span className="title-info">
+                                    <FaCalendarAlt/> Lançamento
+                                </span> {data.released !== null ? (dateFormat(data.released)) : (<span> N/D</span>)}
                             </p>
                         </div>
                         <Link to={`/game/${data.slug}`} className="btn btn-primary btn-block mt-2">Ver mais</Link>
