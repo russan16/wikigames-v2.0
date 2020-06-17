200323d2fb83f7992632e714593abf447964c8d3
Italo Russan <italo.russan@csktech.com.br>
2020-06-17 17:45:02 -0300

    fix: mais info sobre os jogos

--- src/pages/InternaPlataformas/index.jsx
+++ src/pages/InternaPlataformas/index.jsx
@@ -32,25 +32,27 @@ export default function InternaPlataformas(props) {
                 ))}
             </div>
 
-            <nav aria-label="Page navigation" className="mt-5 w-100">
-                <ul className="pagination justify-content-center pagination-lg">
-                    <li className={`page-item ${page === 1 ? 'disabled' : ''}`}>
-                        <button className="page-link" onClick={() => setPage(1)}>1</button>
-                    </li>
-                    <li className={`page-item ${page === 2 ? 'disabled' : ''}`}>
-                        <button className="page-link" onClick={() => setPage(2)}>2</button>
-                    </li>
-                    <li className={`page-item ${page === 3 ? 'disabled' : ''}`}>
-                        <button className="page-link" onClick={() => setPage(3)}>3</button>
-                    </li>
-                    <li className={`page-item ${page === 4 ? 'disabled' : ''}`}>
-                        <button className="page-link" onClick={() => setPage(4)}>4</button>
-                    </li>
-                    <li className={`page-item ${page === 5 ? 'disabled' : ''}`}>
-                        <button className="page-link" onClick={() => setPage(5)}>5</button>
-                    </li>
-                </ul>
-            </nav>
+            {isLoading ? ('') : (
+                <nav aria-label="Page navigation" className="mt-5 w-100">
+                    <ul className="pagination justify-content-center pagination-lg">
+                        <li className={`page-item ${page === 1 ? 'disabled' : ''}`}>
+                            <button className="page-link" onClick={() => setPage(1)}>1</button>
+                        </li>
+                        <li className={`page-item ${page === 2 ? 'disabled' : ''}`}>
+                            <button className="page-link" onClick={() => setPage(2)}>2</button>
+                        </li>
+                        <li className={`page-item ${page === 3 ? 'disabled' : ''}`}>
+                            <button className="page-link" onClick={() => setPage(3)}>3</button>
+                        </li>
+                        <li className={`page-item ${page === 4 ? 'disabled' : ''}`}>
+                            <button className="page-link" onClick={() => setPage(4)}>4</button>
+                        </li>
+                        <li className={`page-item ${page === 5 ? 'disabled' : ''}`}>
+                            <button className="page-link" onClick={() => setPage(5)}>5</button>
+                        </li>
+                    </ul>
+                </nav>
+            )}
             <Loading status={isLoading}/>
         </section>
     );
