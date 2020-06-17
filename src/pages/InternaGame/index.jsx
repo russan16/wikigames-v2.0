cffa03f7191a9b5020b1f4173eb485ab9f6ed003
Italo Russan <italo.russan@csktech.com.br>
2020-06-17 16:29:43 -0300

    add: ajustes na interna do jogo

--- src/pages/InternaGame/index.jsx
+++ src/pages/InternaGame/index.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import api from '../../services/api';
 import {dateFormat} from '../../components/Conversion';
 import Loading from '../../components/Loading';
+import noImg from '../../assets/images/no-img.jpeg';
 
 export default function InternaGame(props) {
 
@@ -19,16 +20,25 @@ export default function InternaGame(props) {
     }, []);
 
     return (
-        <section>
-            <figure>
-                <img className="img-fluid" src={gameInfo.background_image_additional} alt=""/>
-            </figure>
-            <h1 className="text-light">{gameInfo.name}</h1>
-            <p className="text-light">{gameInfo.description_raw}</p>
-            <ul className="list-unstyled">
-                <li className="text-light">Último patch: {dateFormat(gameInfo.released)}</li>
-                <li className="text-light">Site oficial: <a className="text-light" href={gameInfo.website} target="_blank">{gameInfo.website}</a></li>
-            </ul>
+        <section className="row">
+            <div className="col-12">
+                <h1 className="text-light text-center">{gameInfo.name}</h1>
+            </div>
+
+            <div className="col-12">
+                <p className="text-light">
+                    <figure className="figure float-left mr-3 mb-3 w-50">
+                        <img className="img-thumbnail" src={gameInfo.background_image_additional === null ? noImg : gameInfo.background_image_additional} alt=""/>
+                    </figure>
+                    <strong>Descrição:</strong> {gameInfo.description_raw === null ? ('não disponível') : (gameInfo.description_raw)}</p>
+                <ul className="list-unstyled">
+                    <li className="text-light">Último patch: {dateFormat(gameInfo.released)}</li>
+                    {gameInfo.website === '' ? ('') : (
+                        <li className="text-light">Site oficial: <a className="text-light" href={gameInfo.website} target="_blank">{gameInfo.website}</a></li>
+                    )}
+
+                </ul>
+            </div>
             <Loading status={isLoading}/>
         </section>
     );
